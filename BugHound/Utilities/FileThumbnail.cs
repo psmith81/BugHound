600884983a87f1c878f80e4da48e56f01dbbb6d3
psmith81 <psmith81@gmail.com>
2014-11-10 16:34:54 -0500

    Development1110PM
    Messed Up Template

---
+++ BugHound/Utilities/FileThumbnail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugHound.Utilities
+{
+    //public class FileThumbnail (string FileName)
+    //{
+    //    var FileExt = new FileName.LastIndexOf('.');
+    //    return View();
+    //}
+}
+
+//// Extension method
+//public static MvcHtmlString ActionImage(this HtmlHelper html, string action, object routeValues, string imagePath, string alt)
+//{
+//    var url = new UrlHelper(html.ViewContext.RequestContext);
+
+//    // build the <img> tag
+//    var imgBuilder = new TagBuilder("img");
+//    imgBuilder.MergeAttribute("src", url.Content(imagePath));
+//    imgBuilder.MergeAttribute("alt", alt);
+//    string imgHtml = imgBuilder.ToString(TagRenderMode.SelfClosing);
+
+//    // build the <a> tag
+//    var anchorBuilder = new TagBuilder("a");
+//    anchorBuilder.MergeAttribute("href", url.Action(action, routeValues));
+//    anchorBuilder.InnerHtml = imgHtml; // include the <img> tag inside
+//    string anchorHtml = anchorBuilder.ToString(TagRenderMode.Normal);
+
+//    return MvcHtmlString.Create(anchorHtml);
+//}
